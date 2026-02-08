bb94243fcdc959b0ff458fd45746107f56cc9980
TechChiru007 <129850601+TechChiru007@users.noreply.github.com>
2026-02-08 12:25:05 +0530

    Change loop condition from 5 to 4 iterations

--- Others/Notes.java
+++ Others/Notes.java
@@ -1,6 +1,6 @@
 public class Notes{
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             System.out.println("Hello, World!");
         }
     }
